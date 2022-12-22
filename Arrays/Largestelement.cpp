470c56f36534769d70915bd7dae934538e7c2931
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-12-22 20:09:19 +0530

    DSA

--- Arrays/Largestelement.cpp
+++ Arrays/Largestelement.cpp
@@ -1,24 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Time Complexity is Theta(n)
+
 int getLargest(int arr[], int n){
+    int res = 0;
     for (int i = 0; i < n; i++)
     {
-        bool flag = true;
-        for (int j = 0; j < n; j++)
-        {
-            if (arr[j] > arr[i])
-            {
-                flag = false;
-                break;
-            }  
+        if(arr[i] > arr[res]){
+            res = i;
         }
-        if(flag == true){
-            return i;
-        }
+        return res;
     }
-        return -1;
-    }
+    
+}
+
+// Time Complexity is O(n^2)
+
+// int getLargest(int arr[], int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         bool flag = true;
+//         for (int j = 0; j < n; j++)
+//         {
+//             if (arr[j] > arr[i])
+//             {
+//                 flag = false;
+//                 break;
+//             }  
+//         }
+//         if(flag == true){
+//             return i;
+//         }
+//     }
+//         return -1;
+//     }
 
     int main()
     {
