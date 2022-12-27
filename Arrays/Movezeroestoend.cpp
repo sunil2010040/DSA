fb7843bf30c3097e1089317960aa54dbb9258dcd
Sunil Kumar Chakravartty <91359079+sunil2010040@users.noreply.github.com>
2022-12-27 13:39:20 +0530

    Update Movezeroestoend.cpp

--- Arrays/Movezeroestoend.cpp
+++ Arrays/Movezeroestoend.cpp
@@ -1,37 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// int moveToend(int arr[], int n)
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (arr[i] == 0)
-//         {
-//             for (int j = i + 1; j < n; j++)
-//             {
-//                 if (arr[j] != 0)
-//                 {
-//                     swap(arr[i], arr[j]);
-//                 }
-//             }
-//             for (int i = 0; i < n; i++)
-//             {
-//                 return arr[i];
-//             }
-//         }
-//     }
-// }
-
-int main()
+void move_zero_to_end(int arr[], int n)
 {
-
-    int n, arr[n];
-    cin >> n;
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> arr[i];
-    }
-
     for (int i = 0; i < n; i++)
     {
         if (arr[i] == 0)
@@ -45,9 +16,24 @@ int main()
             }
         }
     }
-    for (int i = 0; i < n; ++i)
+    
+    for(int i=0;i<n;i++)
     {
-        cout << arr[i] << " ";
+        cout<<arr[i]<<" ";
     }
+}
+
+int main()
+{
+    int n;
+    cin >> n; 
+    int arr[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    move_zero_to_end(arr,n);
     return 0;
 }
