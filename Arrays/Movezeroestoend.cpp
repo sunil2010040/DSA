a7a58ea71ef4a93d0a962a5245fd1a16f41f3cb8
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2022-12-27 13:11:42 +0530

    DSA

--- Arrays/Movezeroestoend.cpp
+++ Arrays/Movezeroestoend.cpp
@@ -1,8 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int moveToend(int arr[], int n)
+// int moveToend(int arr[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == 0)
+//         {
+//             for (int j = i + 1; j < n; j++)
+//             {
+//                 if (arr[j] != 0)
+//                 {
+//                     swap(arr[i], arr[j]);
+//                 }
+//             }
+//             for (int i = 0; i < n; i++)
+//             {
+//                 return arr[i];
+//             }
+//         }
+//     }
+// }
+
+int main()
 {
+
+    int n, arr[n];
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
     for (int i = 0; i < n; i++)
     {
         if (arr[i] == 0)
@@ -14,25 +43,11 @@ int moveToend(int arr[], int n)
                     swap(arr[i], arr[j]);
                 }
             }
-            for (int i = 0; i < n; i++)
-            {
-                return arr[i];
-            }
         }
     }
-}
-
-int main()
-{
-    int r, numbers[r];
-    cin >> r;
-    for (int i = 0; i < r; ++i)
+    for (int i = 0; i < n; ++i)
     {
-        cin >> numbers[i];
-    }
-    for (int i = 0; i < r; ++i)
-    {
-        cout << moveToend(numbers, r) << " ";
+        cout << arr[i] << " ";
     }
     return 0;
 }
